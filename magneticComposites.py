fd9d4e430a926ead5cdcabcc380519a484a4b505
András Vukics <vukics.andras@wigner.mta.hu>
2017-02-03 13:27:24 +0100

    Factored off magneticComposites form magneticElements

---
+++ magneticComposites.py
@@ -0,0 +1,235 @@
+import magneticElements
+
+import numpy as np
+
+import math
+
+
+def nz(n) : return np.array((0, 0, 1 if n else -1))
+
+
+ArrayOfSources=magneticElements.ArrayOfSources
+
+
+class CoilNew(magneticElements.CylindricallySymmetricSolid) :
+    """
+    Coil with rectangular cross section
+    
+    It is now implemented in terms of a CylindricallySymmetricSolid rather than ArrayOfSources
+    """
+    
+    def __init__(self,n,r0,R,w,nw,h,nh) :
+        """
+        Arguments
+        ----------
+            n: bool
+                True if direction vector points upwards
+            r0: ndarray, shape (3, )
+                The location of the centre of the trap
+            R: float
+                Inner radius
+            w: float
+                The width of the rectangular cross section
+            nw: float
+                Number of loops along width
+            h: float
+                The height of the rectangular cross section
+            nh: float
+                Number of loops along height
+        
+        There are no relative currents here, as the same current will flow in all loops
+        """
+        super(CoilNew,self).__init__(n,r0)
+
+        self.loops=[magneticElements.CurrentLoop(n,r0+i*nz(n),Rj) for i in np.linspace(-h/2.,h/2.,nh) for Rj in np.linspace(R,R+w,nw)]
+
+
+    def calculateFieldInOwnCylindricalCoordinates(self,rho,phi,z) :
+        midres=self.loops[0].calculateFieldInOwnCylindricalCoordinates(rho,phi,z)
+        Brho=midres[0]; Bz=midres[2]
+        for source in self.loops[1:] :
+            midres=source.calculateFieldInOwnCylindricalCoordinates(rho,phi,z)
+            Brho+=midres[0]; Bz+=midres[2]
+
+        ZERO = np.zeros(Brho.shape)
+        return (Brho, ZERO, Bz)
+
+
+
+class Coil(ArrayOfSources) :
+    """
+    Coil with rectangular cross section
+    
+    Here, this is a simple array of current loops, so we completely disregard the fact that the transition matrices will be the same for all loops, that is, that Coil is itself a CylindricallySymmetricSolid.
+    This is because multiple inheritance is rather tiresome in python.
+    Hope this won’t cause too big an overhead…
+    """
+    
+    def __init__(self,n,r0,R,w,nw,h,nh) :
+        """
+        Arguments
+        ----------
+            n: bool
+                True if direction vector points upwards
+            r0: ndarray, shape (3, )
+                The location of the centre of the trap
+            R: float
+                Inner radius
+            w: float
+                The width of the rectangular cross section
+            nw: float
+                Number of loops along width
+            h: float
+                The height of the rectangular cross section
+            nh: float
+                Number of loops along height
+        
+        There are no relative currents here, as the same current will flow in all loops
+        """
+        super(Coil,self).__init__([magneticElements.CurrentLoop(n,r0+i*nz(n),Rj) for i in np.linspace(-h/2.,h/2.,nh) for Rj in np.linspace(R,R+w,nw)])
+
+
+
+class TwoCoils(ArrayOfSources) :
+    
+    def __init__(self,n1,n2,r0,R,w,nw,h,nh,d,relativeCurrents=None) :
+        """
+        Arguments
+        ---------
+            n1, n2 : the current orientation in Coil 1 and 2, respectively
+            r0, R, w, nw, h, nh: same as for Coil
+            d: float
+                The distance of the two coils
+        """
+        r0=np.array(r0)
+        super(TwoCoils,self).__init__([
+            Coil(n1,r0+d/2.*nz(n1),R,w,nw,h,nh),
+            Coil(n2,r0-d/2.*nz(n1),R,w,nw,h,nh)
+            ],relativeCurrents)
+        
+        self.d = d
+
+    
+    
+class DipoleCoils(TwoCoils) :
+    """
+    Two instances of Coil with the same axis and same current directions
+    """
+    
+    def __init__(self,n,r0,R,w,nw,h,nh,d,relativeCurrents=None) :
+        """
+        Arguments
+        ---------
+            n, r0, R, w, nw, h, nh: same as for Coil
+            d: float
+                The distance of the two coils
+        """
+        super(DipoleCoils,self).__init__(n,n,r0,R,w,nw,h,nh,d,relativeCurrents)
+
+
+
+class QuadrupoleTrap(TwoCoils) :
+    """
+    Two instances of Coil with the same axis but opposite current directions
+    """
+    
+    def __init__(self,n,r0,R,w,nw,h,nh,d,relativeCurrents=None) :
+        """
+        Arguments
+        ---------
+            n, r0, R, w, nw, h, nh: same as for Coil
+            d: float
+                The distance of the two coils
+        """
+        super(QuadrupoleTrap,self).__init__(n,not n,r0,R,w,nw,h,nh,d,relativeCurrents)
+
+
+
+class IoffeWires(ArrayOfSources) :
+    """
+    Two instances of InfiniteWire with opposite orientation
+    """
+    
+    def __init__(self,n,r0,m,d,rhoLimit,relativeCurrents=None) :
+        """
+        Arguments
+        ---------
+            n: bool
+                True if direction vector points upwards
+            r0: ndarray, shape (3, )
+                The location of the centre
+            m: ndarray, shape (3, )
+                Direction vector of the relative positions
+            d: float
+                The distance of the two wires
+        """
+        r0=np.array(r0); m=np.array(m)
+        super(IoffeWires,self).__init__([
+            magneticElements.InfiniteWire(    n,r0+d/2.*m,rhoLimit),
+            magneticElements.InfiniteWire(not n,r0-d/2.*m,rhoLimit)
+            ],relativeCurrents)
+
+
+
+class QuadrupoleSimplex(ArrayOfSources) :
+    """
+    From two loops mostly for testing
+    """
+    def __init__(self,n,r0,R,d,relativeCurrents=None) :
+        """
+        Arguments
+        ---------
+            n, r0, R: same as for CurrentLoop
+            d: float
+                The distance of the two loops
+        """
+        r0=np.array(r0)
+        super(QuadrupoleSimplex,self).__init__([
+            magneticElements.CurrentLoop(    n,r0+d/2.*nz(n),R),
+            magneticElements.CurrentLoop(not n,r0-d/2.*nz(n),R)
+            ],relativeCurrents)
+        
+        self.d = d
+    
+    
+    def coefficient(self) :
+        A = self.d/2
+        R = self.arrayOfSources[0].R
+        return 3*math.pi*A*R**2/(R**2+A**2)**2.5, 5*math.pi*(4*A**2-3*R**2)/(6*(A**2+R**2)**2)
+
+
+
+class HelmholtzCoil(ArrayOfSources) :
+    def __init__(self,n,r0,R) :
+        r0=np.array(r0)
+        super(HelmholtzCoil,self).__init__([
+            magneticElements.CurrentLoop(n,r0+R/2.*nz(n),R),
+            magneticElements.CurrentLoop(n,r0-R/2.*nz(n),R)
+            ])
+
+
+
+class InfiniteWireWithSquareCrossSection(ArrayOfSources) :
+    """
+    Implemented as an array of 5 point-like wires
+    """
+    def __init__(self,n,r0,direction,rhoLimit) :
+        """
+        Arguments
+        ----------
+            n: bool
+                True if direction vector points upwards
+            r0: ndarray, shape (2, )
+                The location of the centre of the square in units of d: [x y]
+            direction: ndarray, shape (2, )
+                The location vector of any of the sides of the square from the centre in units of d: [x y]
+            rhoLimit: float
+                Minimal rho to calculate field for
+        """
+        r0=r0[:2]
+        direction=direction[:2]; directionRot=np.array((-direction[1],direction[0]))
+        diagonal=direction+directionRot; diagonalRot=direction-directionRot
+        super(InfiniteWireWithSquareCrossSection,self).__init__([magneticElements.InfiniteWire(n,rIter,rhoLimit) for rIter in [r0,r0+diagonal,r0-diagonal,r0+diagonalRot,r0-diagonalRot]],
+                                                                0.2*np.ones(5)
+                                                                )
+        self.rhoLimit=rhoLimit
--- magneticElements.py
+++ magneticElements.py
@@ -4,15 +4,11 @@ from scipy import special
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-import math
-
 
 
 def create3by3matrix(a00,a01,a02,a10,a11,a12,a20,a21,a22) : return np.array((np.array((a00,a01,a02)), np.array((a10,a11,a12)), np.array((a20,a21,a22))))
 
 def matrixVector(m,v) : return np.einsum('ij...,j...->i...',m,v)
-
-def nz(n) : return np.array((0, 0, 1 if n else -1))
 
 def filterIndeces(a) :
     try :
@@ -183,51 +179,6 @@ class InfiniteWire(CylindricallySymmetricSolid) :
 
 
 
-class CoilNew(CylindricallySymmetricSolid) :
-    """
-    Coil with rectangular cross section
-    
-    It is now implemented in terms of a CylindricallySymmetricSolid rather than ArrayOfSources
-    """
-    
-    def __init__(self,n,r0,R,w,nw,h,nh) :
-        """
-        Arguments
-        ----------
-            n: bool
-                True if direction vector points upwards
-            r0: ndarray, shape (3, )
-                The location of the centre of the trap
-            R: float
-                Inner radius
-            w: float
-                The width of the rectangular cross section
-            nw: float
-                Number of loops along width
-            h: float
-                The height of the rectangular cross section
-            nh: float
-                Number of loops along height
-        
-        There are no relative currents here, as the same current will flow in all loops
-        """
-        super(CoilNew,self).__init__(n,r0)
-
-        self.loops=[CurrentLoop(n,r0+i*nz(n),Rj) for i in np.linspace(-h/2.,h/2.,nh) for Rj in np.linspace(R,R+w,nw)]
-
-
-    def calculateFieldInOwnCylindricalCoordinates(self,rho,phi,z) :
-        midres=self.loops[0].calculateFieldInOwnCylindricalCoordinates(rho,phi,z)
-        Brho=midres[0]; Bz=midres[2]
-        for source in self.loops[1:] :
-            midres=source.calculateFieldInOwnCylindricalCoordinates(rho,phi,z)
-            Brho+=midres[0]; Bz+=midres[2]
-
-        ZERO = np.zeros(Brho.shape)
-        return (Brho, ZERO, Bz)
-
-
-
 class ArrayOfSources(object) :
     """
     Adds up the fields of its elements
@@ -261,176 +212,6 @@ class ArrayOfSources(object) :
         for source, I in zip(self.arrayOfSources[1:],self.relativeCurrents[1:]) :
             res+=source.thermalPowerCoeff(r)*I**2
         return res
-
-
-
-class InfiniteWireWithSquareCrossSection(ArrayOfSources) :
-    """
-    Implemented as an array of 5 point-like wires
-    """
-    def __init__(self,n,r0,direction,rhoLimit) :
-        """
-        Arguments
-        ----------
-            n: bool
-                True if direction vector points upwards
-            r0: ndarray, shape (2, )
-                The location of the centre of the square in units of d: [x y]
-            direction: ndarray, shape (2, )
-                The location vector of any of the sides of the square from the centre in units of d: [x y]
-            rhoLimit: float
-                Minimal rho to calculate field for
-        """
-        r0=r0[:2]
-        direction=direction[:2]; directionRot=np.array((-direction[1],direction[0]))
-        diagonal=direction+directionRot; diagonalRot=direction-directionRot
-        super(InfiniteWireWithSquareCrossSection,self).__init__([InfiniteWire(n,rIter,rhoLimit) for rIter in [r0,r0+diagonal,r0-diagonal,r0+diagonalRot,r0-diagonalRot]],
-                                                                0.2*np.ones(5)
-                                                                )
-        self.rhoLimit=rhoLimit
-
-
-
-class Coil(ArrayOfSources) :
-    """
-    Coil with rectangular cross section
-    
-    Here, this is a simple array of current loops, so we completely disregard the fact that the transition matrices will be the same for all loops, that is, that Coil is itself a CylindricallySymmetricSolid.
-    This is because multiple inheritance is rather tiresome in python.
-    Hope this won’t cause too big an overhead…
-    """
-    
-    def __init__(self,n,r0,R,w,nw,h,nh) :
-        """
-        Arguments
-        ----------
-            n: bool
-                True if direction vector points upwards
-            r0: ndarray, shape (3, )
-                The location of the centre of the trap
-            R: float
-                Inner radius
-            w: float
-                The width of the rectangular cross section
-            nw: float
-                Number of loops along width
-            h: float
-                The height of the rectangular cross section
-            nh: float
-                Number of loops along height
-        
-        There are no relative currents here, as the same current will flow in all loops
-        """
-        super(Coil,self).__init__([CurrentLoop(n,r0+i*nz(n),Rj) for i in np.linspace(-h/2.,h/2.,nh) for Rj in np.linspace(R,R+w,nw)])
-
-
-
-class TwoCoils(ArrayOfSources) :
-    
-    def __init__(self,n1,n2,r0,R,w,nw,h,nh,d,relativeCurrents=None) :
-        """
-        Arguments
-        ---------
-            n1, n2 : the current orientation in Coil 1 and 2, respectively
-            r0, R, w, nw, h, nh: same as for Coil
-            d: float
-                The distance of the two coils
-        """
-        r0=np.array(r0)
-        super(TwoCoils,self).__init__([
-            Coil(n1,r0+d/2.*nz(n1),R,w,nw,h,nh),
-            Coil(n2,r0-d/2.*nz(n1),R,w,nw,h,nh)
-            ],relativeCurrents)
-        
-        self.d = d
-
-    
-    
-class QuadrupoleTrap(TwoCoils) :
-    """
-    Two instances of Coil with the same axis but opposite current directions
-    """
-    
-    def __init__(self,n,r0,R,w,nw,h,nh,d,relativeCurrents=None) :
-        """
-        Arguments
-        ---------
-            n, r0, R, w, nw, h, nh: same as for Coil
-            d: float
-                The distance of the two coils
-        """
-        super(QuadrupoleTrap,self).__init__(n,not n,r0,R,w,nw,h,nh,d,relativeCurrents)
-
-
-
-class DipoleCoils(TwoCoils) :
-    """
-    Two instances of Coil with the same axis and same current directions
-    """
-    
-    def __init__(self,n,r0,R,w,nw,h,nh,d,relativeCurrents=None) :
-        """
-        Arguments
-        ---------
-            n, r0, R, w, nw, h, nh: same as for Coil
-            d: float
-                The distance of the two coils
-        """
-        super(DipoleCoils,self).__init__(n,n,r0,R,w,nw,h,nh,d,relativeCurrents)
-
-
-
-class IoffeWires(ArrayOfSources) :
-    """
-    Two instances of InfiniteWire with opposite orientation
-    """
-    
-    def __init__(self,n,r0,m,d,rhoLimit,relativeCurrents=None) :
-        """
-        Arguments
-        ---------
-            n: bool
-                True if direction vector points upwards
-            r0: ndarray, shape (3, )
-                The location of the centre
-            m: ndarray, shape (3, )
-                Direction vector of the relative positions
-            d: float
-                The distance of the two wires
-        """
-        r0=np.array(r0); m=np.array(m)
-        super(IoffeWires,self).__init__([
-            InfiniteWire(    n,r0+d/2.*m,rhoLimit),
-            InfiniteWire(not n,r0-d/2.*m,rhoLimit)
-            ],relativeCurrents)
-
-
-
-class QuadrupoleSimplex(ArrayOfSources) :
-    """
-    From two loops mostly for testing
-    """
-    def __init__(self,n,r0,R,d,relativeCurrents=None) :
-        """
-        Arguments
-        ---------
-            n, r0, R: same as for CurrentLoop
-            d: float
-                The distance of the two loops
-        """
-        r0=np.array(r0)
-        super(QuadrupoleSimplex,self).__init__([
-            CurrentLoop(    n,r0+d/2.*nz(n),R),
-            CurrentLoop(not n,r0-d/2.*nz(n),R)
-            ],relativeCurrents)
-        
-        self.d = d
-    
-    
-    def coefficient(self) :
-        A = self.d/2
-        R = self.arrayOfSources[0].R
-        return 3*math.pi*A*R**2/(R**2+A**2)**2.5, 5*math.pi*(4*A**2-3*R**2)/(6*(A**2+R**2)**2)
 
 
 
@@ -478,12 +259,3 @@ def gradientOfNorm(fieldDeriv) :
     field=fieldDeriv[0]
     return matrixVector(fieldDeriv[1],field)/np.linalg.norm(field,axis=0)
 
-
-
-class HelmholtzCoil(ArrayOfSources) :
-    def __init__(self,n,r0,R) :
-        r0=np.array(r0)
-        super(HelmholtzCoil,self).__init__([
-            CurrentLoop(n,r0+R/2.*nz(n),R),
-            CurrentLoop(n,r0-R/2.*nz(n),R)
-            ])
